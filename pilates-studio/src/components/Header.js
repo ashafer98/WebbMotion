f27fb56fbbba4799087af585f9ff58b72aa95833
Anya Shafer <anya.shafer@gmail.com>
2024-12-16 21:03:30 -0500

    updated nav bar

--- pilates-studio/src/components/Header.js
+++ pilates-studio/src/components/Header.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 
 
 function Header() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">Webb Motion</Link>
-      </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/classes">Classes</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-export default Header;
-
+    const [menuOpen, setMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+  
+    return (
+      <header className="header">
+        <div className="logo">
+          <Link to="/">Webb Motion</Link>
+        </div>
+        <nav className={`nav-links ${menuOpen ? 'nav-active' : ''}`}>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/classes">Classes</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
